83f3bdbcd732a50ed87ed4e6d55961d399fbe9fa
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-05-22 18:06:58 +0200

    re

--- src/components/06-records.js
+++ src/components/06-records.js
@@ -53,7 +53,7 @@ export function records() {
         .style('opacity', 0); // Invisible au début
 
     legoMan.append('img')
-        .attr('src', 'public/images/lego-figure.png')
+        .attr('src', 'dist/images/lego-figure.png')
         .attr('alt', 'Bonhomme LEGO');
 
     // Ajout de la tour (SVG)
